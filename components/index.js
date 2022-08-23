f8f29e2659f9e44c3be32b7042770219888cf48d
JackWorld <jackwong0326@gmail.com>
2022-08-23 22:39:42 +0800

    Add About components.

--- components/index.js
+++ components/index.js
@@ -1,2 +1,3 @@
 export { default as Sildebar } from './Sildebar';
 export { default as Navbar } from './Navbar';
+export { default as About } from './About';
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Sildebar, Navbar } from '../components'
+import { Sildebar, Navbar, About } from '../components'
 
 export default function Home() {
   return (
@@ -7,6 +7,7 @@ export default function Home() {
 
       <div className="main-content">
         <Navbar />
+        <About />
       </div>
     </>
   )
