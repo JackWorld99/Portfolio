c2c1e8d097b18895d4c41f5bdfc18170bd00fa2a
JackWorld <jackwong0326@gmail.com>
2022-08-24 18:15:15 +0800

    Add Portfolio components.

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Sildebar } from './Sildebar';
 export { default as Navbar } from './Navbar';
 export { default as About } from './About';
 export { default as Resume } from './Resume';
+export { default as Portfolio } from './Portfolio';
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Sildebar, Navbar, About, Resume} from '../components'
+import { Sildebar, Navbar, About, Resume, Portfolio } from '../components'
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
         <Navbar />
         <About />
         <Resume />
+        <Portfolio />
       </div>
     </>
   )
