8c9f38822a193abe536a69927221ad8c427bcdc3
JackWorld <jackwong0326@gmail.com>
2022-08-27 03:29:50 +0800

    Filter project base on category.

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,9 +1,36 @@
+import React, {useState } from "react"
 import { IoChevronDown, IoEyeOutline } from "react-icons/io5"
 import { useStateContext } from "../context/StateContext"
 
 const Portfolio = () => {
+    const projectCategory = [{id: 1, name: "All"},{id: 2,name: "Web Design"}, {id: 3, name: "Applications"}, {id: 4, name: "Web Development"}]
+    const projectList = [
+        {id: 1, name: "Finance", category: "Web Development", image:"project-1.jpg", show: true},
+        {id: 2, name: "Orizon", category: "Web Development", image:"project-2.png", show: true},
+        {id: 3, name: "Fundo", category: "Web Design", image:"project-3.jpg", show: true},
+        {id: 4, name: "Brawlhalla", category: "Applications", image:"project-4.png", show: true},
+        {id: 5, name: "DSM.", category: "Web Design", image:"project-5.png", show: true},
+        {id: 6, name: "MetaSpark", category: "Web Design", image:"project-6.png", show: true},
+        {id: 7, name: "Summary", category: "Web Development", image:"project-7.png", show: true},
+        {id: 8, name: "Task Manager", category: "Applications", image:"project-8.jpg", show: true},
+        {id: 9, name: "Arrival", category: "Web Development", image:"project-9.png", show: true},
+    ]
+
+    const [click, setClick] = useState(false)
+    const [select, setSelect] = useState("Select category")
+    const [clickColor, setClickColor] = useState("All")
+    const [projects, setProjects] = useState(projectList)
     const {page} = useStateContext()
     
+
+    const handleClick = (e) => {
+        const selectedCategory = e.target.innerText
+        setClick(false)
+        setClickColor(selectedCategory)
+        setSelect(selectedCategory)
+        setProjects(projects.map(project => (selectedCategory === "All" || project.category === selectedCategory) ? {...project, show: true } : {...project, show: false }))
+    }
+
     return (
         <article className={`portfolio ${page === "portfolio" ? "active" : ""}`}>
             <header>
@@ -11,161 +38,45 @@ const Portfolio = () => {
             </header>
             <section className="projects">
                 <ul className="filter-list">
-                    <li className="filter-item">
-                        <button className="active" data-filter-btn>All</button>
-                    </li>
-                    <li className="filter-item">
-                        <button data-filter-btn>Web design</button>
-                    </li>
-                    <li className="filter-item">
-                        <button data-filter-btn>Applications</button>
-                    </li>
-                    <li className="filter-item">
-                        <button data-filter-btn>Web development</button>
-                    </li>
+                    {projectCategory.map((pc) => (
+                        <li className="filter-item" key={pc.id}>
+                            <button className={clickColor === pc.name ? "active" : ""} onClick={handleClick}>{pc.name}</button>
+                        </li>
+                    ))}
                 </ul>
 
-                <div className="filter-select-box">
-                    <button className="filter-select" data-select>
-                        <div className="select-value" data-selecct-value>Select category</div>
+                <div className="filter-select-box">-
+                    <button className={`filter-select ${click ? "active" : ""}`} onClick={() => setClick(!click)}>
+                        <div className="select-value">{select}</div>
                         <div className="select-icon">
                             <IoChevronDown className="ion-icon"/>
                         </div>
                     </button>
 
                     <ul className="select-list">
-                        <li className="select-item">
-                            <button data-select-item>All</button>
-                        </li>
-                        <li className="select-item">
-                            <button data-select-item>Web design</button>
-                        </li>
-                        <li className="select-item">
-                            <button data-select-item>Applications</button>
-                        </li>
-                        <li className="select-item">
-                            <button data-select-item>Web development</button>
-                        </li>
+                        {projectCategory.map((pc) => (
+                            <li className="select-item" key={pc.id}> 
+                                <button onClick={handleClick}>{pc.name}</button>
+                            </li>
+                        ))}
                     </ul>
                 </div>
 
                 <ul className="project-list">
-                    <li className="project-item  active" data-filter-item data-category="web development">
-                        <a href="#">
-                            <figure className="project-img">
-                                <div className="project-item-icon-box">
-                                    <IoEyeOutline className="ion-icon"/>
-                                </div>
-                                <img src="/images/project-1.jpg" alt="finance" loading="lazy" />
-                            </figure>
-                            <h3 className="project-title">Finance</h3>
-                            <p className="project-category">Web development</p>
-                        </a>
-                    </li>
-
-                    <li className="project-item  active" data-filter-item data-category="web development">
-                        <a href="#">
-                            <figure className="project-img">
-                                <div className="project-item-icon-box">
-                                    <IoEyeOutline className="ion-icon"/>
-                                </div>
-                                <img src="/images/project-2.png" alt="orizon" loading="lazy" />
-                            </figure>
-                            <h3 className="project-title">Orizon</h3>
-                            <p className="project-category">Web development</p>
-                        </a>
-                    </li>
-
-                    <li className="project-item  active" data-filter-item data-category="web design">
-                        <a href="#">
-                            <figure className="project-img">
-                                <div className="project-item-icon-box">
-                                    <IoEyeOutline className="ion-icon"/>
-                                </div>
-                                <img src="/images/project-3.jpg" alt="fundo" loading="lazy" />
-                            </figure>
-                            <h3 className="project-title">Fundo</h3>
-                            <p className="project-category">Web design</p>
-                        </a>
-                    </li>
-
-                    <li className="project-item  active" data-filter-item data-category="applications">
-                        <a href="#">
-                            <figure className="project-img">
-                                <div className="project-item-icon-box">
-                                    <IoEyeOutline className="ion-icon"/>
-                                </div>
-                                <img src="/images/project-4.png" alt="brawlhalla" loading="lazy" />
-                            </figure>
-                            <h3 className="project-title">Brawlhalla</h3>
-                            <p className="project-category">Applications</p>
-                        </a>
-                    </li>
-
-                    <li className="project-item  active" data-filter-item data-category="web design">
-                        <a href="#">
-                            <figure className="project-img">
-                                <div className="project-item-icon-box">
-                                    <IoEyeOutline className="ion-icon"/>
-                                </div>
-                                <img src="/images/project-5.png" alt="dsm." loading="lazy" />
-                            </figure>
-                            <h3 className="project-title">DSM.</h3>
-                            <p className="project-category">Web design</p>
-                        </a>
-                    </li>
-
-                    <li className="project-item  active" data-filter-item data-category="web design">
-                        <a href="#">
-                            <figure className="project-img">
-                                <div className="project-item-icon-box">
-                                    <IoEyeOutline className="ion-icon"/>
-                                </div>
-                                <img src="/images/project-6.png" alt="metaspark" loading="lazy" />
-                            </figure>
-                            <h3 className="project-title">MetaSpark</h3>
-                            <p className="project-category">Web design</p>
-                        </a>
-                    </li>
-
-                    <li className="project-item  active" data-filter-item data-category="web development">
-                        <a href="#">
-                            <figure className="project-img">
-                                <div className="project-item-icon-box">
-                                    <IoEyeOutline className="ion-icon"/>
-                                </div>
-                                <img src="/images/project-7.png" alt="summary" loading="lazy" />
-                            </figure>
-                            <h3 className="project-title">Summary</h3>
-                            <p className="project-category">Web development</p>
-                        </a>
-                    </li>
-
-                    <li className="project-item  active" data-filter-item data-category="applications">
-                        <a href="#">
-                            <figure className="project-img">
-                                <div className="project-item-icon-box">
-                                    <IoEyeOutline className="ion-icon"/>
-                                </div>
-                                <img src="/images/project-8.jpg" alt="task manager" loading="lazy" />
-                            </figure>
-                            <h3 className="project-title">Task Manager</h3>
-                            <p className="project-category">Applications</p>
-                        </a>
-                    </li>
-
-                    <li className="project-item  active" data-filter-item data-category="web development">
-                        <a href="#">
-                            <figure className="project-img">
-                                <div className="project-item-icon-box">
-                                    <IoEyeOutline className="ion-icon"/>
-                                </div>
-                                <img src="/images/project-9.png" alt="arrival" loading="lazy" />
-                            </figure>
-                            <h3 className="project-title">Arrival</h3>
-                            <p className="project-category">Web development</p>
-                        </a>
-                    </li>
+                    {projects.map((project) => (
+                        <li className={`project-item ${project.show ? "active" : ""}`} key={project.id}>
+                            <a href="#">
+                                <figure className="project-img">
+                                    <div className="project-item-icon-box">
+                                        <IoEyeOutline className="ion-icon"/>
+                                    </div>
+                                    <img src={`/images/${project.image}`} alt={project.name.toLocaleLowerCase()} loading="lazy" />
+                                </figure>
+                                <h3 className="project-title">{project.name}</h3>
+                                <p className="project-category">{project.category}</p>
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </section>
         </article>
