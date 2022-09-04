857dec9162e293585f182477cd5992f2268e963b
JackWorld <jackwong0326@gmail.com>
2022-09-04 23:12:04 +0800

    Update About page details.

--- components/About.js
+++ components/About.js
@@ -1,4 +1,5 @@
 import React, {useRef, useState} from "react"
+import { IoMdDownload } from "react-icons/io"
 import { IoCloseOutline } from "react-icons/io5"
 import { useStateContext } from "../context/StateContext"
 import useSWR from 'swr'
@@ -36,17 +37,20 @@ const About = () => {
             </header>
             <section className="about-text">
                 <p>
-                    I&apos;m Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media.
-                    I enjoy
-                    turning complex problems into simple, beautiful and intuitive designs.
+                    Hi, I&apos;m Wei Jian, you can me Jack as well. I&apos;m a rookie fresh out of Software Engineering. I&apos;m an ordinary person, a delusional person who wants to change the world with code.
                 </p>
                 <p>
-                    My job is to build your website so that it is functional and user-friendly but at the same time attractive.
-                    Moreover, I
-                    add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring
-                    across your
-                    message and identity in the most creative way. I created web design for many famous brand companies.
+                    I&apos;m a inquisitive person, and when I come across a new technology or a better code algorithm, I try to find as much information as I can. I love reading books because they contain the experience and wisdom of different authors who have spent their entire lives condensing them into one book. I was often troubled by finding books, so I developed a book-scraping application. I used to stay up all day and night to study web scraping. I am the kind of person who will forget to sleep and eat when I find something new and interesting. It sounds crazy. , but I feel very satisfied when the library is developed, I enjoy the feeling of finding problems in life and turning ideas into reality. I love challenges and learning, and I enjoy the process of going from noob to proficient. Learn new things and go in different directions. Because of this, I know a little bit of everything but not proficient. I am dedicated to making complex things simple. I don&apos;t like to waste time, I always want to use the shortest time to get things done, and then leave the rest of the time for myself to do what I want to do. After all, life is too short and my motto is to be happy and enjoy life every day. <br/>
                 </p>
+                <p>
+                    I&apos;m a inquisitive person, when I encounter new technologies or better code algorithms, I will research day and night to find as much information as possible. I love reading books because they contain the experience and wisdom of different authors who have spent their entire lives condensing them into one book. I was often troubled by finding books, so I developed a book-scraping application. I used to stay up all day and night to study web scraping. I am the kind of person who will forget to sleep and eat when I find something new and interesting. It sounds crazy. , but I feel very satisfied when the library is developed, I enjoy the feeling of finding problems in life and turning ideas into reality. I love challenges and learning, and I enjoy the process of going from noob to proficient. Learn new things and go in different directions. Because of this, I know a little bit of everything but not proficient. I am committed to making complex things simple, the simpler the better. I don&apos;t like to waste time, I always want to use the shortest time to get things done, and then leave the rest of the time for myself to do what I want to do. After all, life is too short and my motto is to be happy and enjoy life every day.
+                {/* 我是一个富有好奇心的人， 遇到新技术或是有更好用的代码算法时，我会尽可能地搜寻更多的信息。我喜欢看书，因为书里涵盖个个不同作者的经验与智慧，他们花了一生的时间将其浓缩成一本书。常常为了找书而苦恼，于是我开发了个抓书应用，为了研究web scraping我曾经一日一夜没睡，我是那种发现好玩的新东西就会废寝忘食的那种人，听起来蛮疯狂的，但当图书馆开发好后我感到非常有满足感，我享受那种在生活中发现问题并将脑子里想法变成现实的感觉。我喜欢挑战喜欢学习，我享受从一窍不通到精通的过程。学习新事物并朝不同方向发展。因为这样以至于我什么都懂一点但是都不精通。我致力于将复杂的东西变简单。我不喜欢浪费时间，我总想着用最短的时间做把事情完成，然后将剩下的时间留给自己做自己想做的事。毕竟人生苦短我的座右铭就是天天开心享受人生。 */}
+                {/* 我是一个平凡人，一个痴心妄想，想靠代码改变世界的人。 */}
+                </p>
+                <button className="form-btn">
+                    <IoMdDownload className="ion-icon"/>
+                    <span>CV</span>
+                </button>
             </section>
             <section className="service">
                 <h3 className="h3 service-title">Expected Career</h3>
@@ -63,56 +67,7 @@ const About = () => {
                     ))}
                 </ul>
             </section>
-            {/* <section className="service">
-                <h3 className="h3 service-title">What i&apos;m doing</h3>
-                <ul className="service-list">
-                    <li className="service-item">
-                        <div className="service-icon-box">
-                            <img src="/images/icon-design.svg" alt="design icon" width="40" />
-                        </div>
-                        <div className="service-content-box">
-                            <h4 className="h4 service-item-title">Web design</h4>
-                            <p className="service-item-text">
-                                The most modern and high-quality design made at a professional level.
-                            </p>
-                        </div>
-                    </li>
-                    <li className="service-item">
-                        <div className="service-icon-box">
-                            <img src="/images/icon-dev.svg" alt="Web development icon" width="40" />
-                        </div>
-                        <div className="service-content-box">
-                            <h4 className="h4 service-item-title">Web development</h4>
-                            <p className="service-item-text">
-                                High-quality development of sites at the professional level.
-                            </p>
-                        </div>
-                    </li>
-                    <li className="service-item">
-                        <div className="service-icon-box">
-                            <img src="/images/icon-app.svg" alt="mobile app icon" width="40" />
-                        </div>
-                        <div className="service-content-box">
-                            <h4 className="h4 service-item-title">Mobile apps</h4>
-                            <p className="service-item-text">
-                                Professional development of applications for iOS and Android.
-                            </p>
-                        </div>
-                    </li>
-                    <li className="service-item">
-                        <div className="service-icon-box">
-                            <img src="/images/icon-photo.svg" alt="camera icon" width="40" />
-                        </div>
-                        <div className="service-content-box">
-                            <h4 className="h4 service-item-title">Photography</h4>
-                            <p className="service-item-text">
-                                I make high-quality photos of any category at a professional level.
-                            </p>
-                        </div>
-                    </li>
-                </ul>
-            </section> */}
-            <section className="testimonials">
+            {/* <section className="testimonials">
                 <h3 className="h3 testimonials-title">Testimonials</h3>
                 <ul className="testimonials-list has-scrollbar">
                     <li className="testimonials-item">
@@ -211,7 +166,7 @@ const About = () => {
                         </div>
                     </div>
                 </section>
-            </div>
+            </div> */}
             {/* <section className="clients">
                 <h3 className="h3 clients-title">Clients</h3>
                 <ul className="clients-list has-scrollbar">
