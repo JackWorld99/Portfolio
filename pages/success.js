6c8f6e7711bccb30d22f08f5ddbfd986185914ca
JackWorld <jackwong0326@gmail.com>
2022-09-26 18:14:09 +0800

    Fix: canvas-confetti module undefine error.

--- pages/success.js
+++ pages/success.js
@@ -1,27 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 import confetti from 'canvas-confetti'
 
 const Success = () => {
-  const duration = 5 * 1000;
-  const animationEnd = Date.now() + duration;
-  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
-  function randomInRange(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-
-  const interval = setInterval(function() {
-    const timeLeft = animationEnd - Date.now();
-
-    if (timeLeft <= 0) {
-      return clearInterval(interval);
+  
+  useEffect(() => {
+    const duration = 5 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
     }
-
-    var particleCount = 50 * (timeLeft / duration);
-    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
-    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
-  }, 250)
+  
+    const interval = setInterval(function() {
+      const timeLeft = animationEnd - Date.now();
+  
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+  
+      var particleCount = 50 * (timeLeft / duration);
+      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 250)
+  }, []);
 
   return (
     <div className="success-wrapper" >
